790d6462ef27a04ac2ee210f795cf41d8496f9e6
darincardin <darincardin@gmail.com>
2020-05-15 19:34:31 -0400

    focus

--- src/Tokenizer/Tokenizer.jsx
+++ src/Tokenizer/Tokenizer.jsx
@@ -11,24 +11,32 @@ class Tokenizer extends React.Component{
 			this.setState({value: e.currentTarget.value})
 		}
 	
-		onEnter = e=>{
+		onEnter = e=>{	
 			if(e.key=="Enter") this.onAdd()
 		}
 	
 		onAdd = () =>{
+
 			var has = this.props.tokens.includes(this.state.value);
-					
+			this.focus()
+			
 			if(this.state.value && !has) {
 				
 				this.props.setState({ [this.props.name]: [...this.props.tokens, this.state.value] })
 
 				this.setState({ value:"", className:"" })
+				
 			}
-			else this.setState({className:'invalid'})
+			else this.setState({className:'invalid'})		
 		}
+		
+		focus = ()=>{
+			setTimeout(()=>{
+				this.nameInput.focus();
+			}, 1);
+		}		
 	
 		onDelete = r =>{	
-
 			var tokens = this.props.tokens;
 		
 			var i = tokens.findIndex(i=>i==r);
@@ -43,7 +51,15 @@ class Tokenizer extends React.Component{
 				<div className="tokenizer" >
 					
 					<div>
-						<input placeholder="Tags..." className='form-control' type="text" value={this.state.value} onChange={this.onChange} onKeyDown={this.onEnter} />
+						<input 
+						ref={ input => { this.nameInput = input}}	
+						placeholder={this.props.placeholder} 
+						className='form-control' 
+						type="text" 
+						value={this.state.value} 
+						onChange={this.onChange} 
+						onKeyDown={this.onEnter} />
+
 						<button type="text" className={"btn btn-primary " + this.state.className} onMouseDown={this.onAdd} >
 							<span>Add</span>
 							<i className="glyphicon glyphicon-ban-circle"></i>
@@ -65,6 +81,14 @@ Tokenizer.propTypes = {
     tokens: PropTypes.array.isRequired,
 	setState: PropTypes.func.isRequired
 };
+
+
+
+Tokenizer.defaultProps = {
+	placeholder: 'Tags...'
+}
+
+
 
 export default Tokenizer;
 	
